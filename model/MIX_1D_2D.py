e1aeb4ecb6ff6efe42c1ff8d2ee5dc7faab122d6
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-07-26 17:51:54 +0700

    Update MIX_1D_2D.py

--- model/MIX_1D_2D.py
+++ model/MIX_1D_2D.py
@@ -49,7 +49,7 @@ def mix_model(opt, cnn_1d_model, resnet_50, lstm_extracted_model, input_1D, inpu
               name='RUL', 
               kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
               bias_regularizer=regularizers.l2(1e-4),
-              activity_regularizer=regularizers.l2(1e-5))(merged_value_0)
+              activity_regularizer=regularizers.l2(1e-5))(merged_value_1)
   return Condition, RUL
   
   
