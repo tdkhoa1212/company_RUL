cc688495f0f0678f3472609b374fdb7aefd77e07
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-02 19:28:29 +0700

    Update LSTM.py

--- model/LSTM.py
+++ model/LSTM.py
@@ -59,12 +59,12 @@ def lstm_model(opt, training=None, inputs=None):
 
   x = MaxPooling1D(pool_size=4, strides=None)(x)
 
-  for i in range(8):
+  for i in range(4):
     x = identity_block(x, kernel_size=3, filters=128, stage=2, block=i, training=training)
 
   x = MaxPooling1D(pool_size=4, strides=None)(x)
 
-  for i in range(36):
+  for i in range(23):
     x = identity_block(x, kernel_size=3, filters=256, stage=3, block=i, training=training)
 
   x = MaxPooling1D(pool_size=4, strides=None)(x)
