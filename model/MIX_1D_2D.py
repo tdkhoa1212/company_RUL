71523d000ad6df90d83977ca1afb9dc8e62a68e1
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-02 19:31:19 +0700

    Update MIX_1D_2D.py

--- model/MIX_1D_2D.py
+++ model/MIX_1D_2D.py
@@ -21,6 +21,8 @@ def TransformerLayer(q, v, k, num_heads=4, training=None):
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(v)
     ma  = MultiHeadAttention(head_size=num_heads, num_heads=num_heads)([q, k, v]) + x
+    ma = BatchNormalization()(ma, training=training)
+    ma = Activation('relu')(ma)
     return ma
 
 def add(x1, x2, x3, training=None):
@@ -53,11 +55,11 @@ def mix_model(opt, cnn_1d_model, resnet_50, lstm_extracted_model, input_1D, inpu
   hidden_out_extracted = network_extracted([input_extracted])
   
   merged_value_0 = TransformerLayer(hidden_out_1D, hidden_out_2D, hidden_out_extracted, 8, training)
-  merged_value_1 = add(hidden_out_1D, merged_value_0, hidden_out_2D, training=training)
+#   merged_value_1 = add(hidden_out_1D, merged_value_0, hidden_out_2D, training=training)
     
   Condition = Dense(3, 
                     activation='softmax', 
-                    name='Condition')(merged_value_1)
+                    name='Condition')(merged_value_0)
   RUL = Dense(1, 
               activation='sigmoid', 
               name='RUL')(merged_value_0)
