f3bc59f095562f8c703337f003f46472fe3708ed
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-04 16:16:41 +0700

    Update LSTM.py

--- model/LSTM.py
+++ model/LSTM.py
@@ -80,15 +80,10 @@ def lstm_model(opt, training=None, inputs=None):
   return m
 
 def lstm_extracted_model(opt, training=None, inputs=None):
-  x = LSTM(128, activation='relu',  
-                return_sequences=True, 
-                kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-                bias_regularizer=regularizers.l2(1e-4),
-                activity_regularizer=regularizers.l2(1e-5))(inputs)
-  x = tf.keras.layers.Bidirectional(LSTM(512, activation='relu',  
+  x = tf.keras.layers.Bidirectional(LSTM(128, activation='relu',  
                 return_sequences=False, 
                 kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                 bias_regularizer=regularizers.l2(1e-4),
-                activity_regularizer=regularizers.l2(1e-5)))(x)
+                activity_regularizer=regularizers.l2(1e-5)))(inputs)
   x = BatchNormalization()(x, training=training)
   return x
