95942153ef59627735bb473234630df61b4a46fb
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-13 21:13:19 +0700

    Update MIX_1D_2D.py

--- model/MIX_1D_2D.py
+++ model/MIX_1D_2D.py
@@ -5,10 +5,27 @@ from tensorflow_addons.layers import MultiHeadAttention
 from keras import layers, regularizers
 import keras.backend as K
 
+# def TransformerLayer(q, v, k, num_heads=4, training=None):
+#     q = tf.keras.layers.Dense(256,   activation='relu',
+#                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+#                                      bias_regularizer=regularizers.l2(1e-4),
+#                                      activity_regularizer=regularizers.l2(1e-5))(q)
+#     q = Dropout(0.1)(q, training=training)
+#     k = tf.keras.layers.Dense(256,   activation='relu',
+#                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+#                                      bias_regularizer=regularizers.l2(1e-4),
+#                                      activity_regularizer=regularizers.l2(1e-5))(k)
+#     k = Dropout(0.1)(k, training=training)
+#     v = tf.keras.layers.Dense(256,   activation='relu',
+#                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+#                                      bias_regularizer=regularizers.l2(1e-4),
+#                                      activity_regularizer=regularizers.l2(1e-5))(v)
+#     v = Dropout(0.1)(v, training=training)
+#     all_ = concatenate((q, k, v))
+#     return all_
+
 def TransformerLayer(q, v, k, num_heads=4, training=None):
     # Transformer layer https://arxiv.org/abs/2010.11929 (LayerNorm layers removed for better performance)
-    # x1 = q
-    # x2 = v
     q = tf.expand_dims(q, axis=-2)
     k = tf.expand_dims(k, axis=-2)
     v = tf.expand_dims(v, axis=-2)
@@ -17,33 +34,19 @@ def TransformerLayer(q, v, k, num_heads=4, training=None):
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(q)
-    # q = Dropout(0.1)(q, training=training)
     k = tf.keras.layers.Dense(256,   activation='relu',
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(k)
-    # k = Dropout(0.1)(k, training=training)
     v = tf.keras.layers.Dense(256,   activation='relu',
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(v)
-    # v = Dropout(0.1)(v, training=training)
-    # all_ = concatenate((q, k, v))
+
     ma  = MultiHeadAttention(head_size=num_heads, num_heads=num_heads)([q, k, v])
     ma = BatchNormalization()(ma, training=training)
     ma = Activation('relu')(ma) 
     ma = tf.keras.layers.GRU(128, return_sequences=False)(ma)
-   
-    # x1 = tf.keras.layers.Dense(128,   activation='relu',
-    #                                  kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-    #                                  bias_regularizer=regularizers.l2(1e-4),
-    #                                  activity_regularizer=regularizers.l2(1e-5))(x1)
-    # x2 = tf.keras.layers.Dense(128,   activation='relu',
-    #                                  kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-    #                                  bias_regularizer=regularizers.l2(1e-4),
-    #                                  activity_regularizer=regularizers.l2(1e-5))(x2)
-    # all_ = ma + x2
-    # all_ = Dropout(0.1)(all_, training=training)
     return ma
 
 
