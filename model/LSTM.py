54907d8a9be167e58d9c26a47c5e322d7c579c4c
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-15 20:43:52 +0700

    Update LSTM.py

--- model/LSTM.py
+++ model/LSTM.py
@@ -4,63 +4,85 @@ from keras.models import Model
 from keras import layers, regularizers
 import keras.backend as K
 
-def TransformerLayer(q, v, k, num_heads=4, training=None):
-    # Transformer layer https://arxiv.org/abs/2010.11929 (LayerNorm layers removed for better performance)
 
-    x1 = tf.keras.layers.Dense(128,   activation='relu',
+def identity_block(input_tensor, kernel_size, filters, stage, block, training):
+    conv_name_base = 'res' + str(stage) + str(block) + '_branch'
+    bn_name_base = 'bn' + str(stage) + str(block) + '_branch'
+
+    x = Conv1D(filters,
+               kernel_size=kernel_size,
+               strides=1,
+               padding='same',
+              kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+              bias_regularizer=regularizers.l2(1e-4),
+              activity_regularizer=regularizers.l2(1e-5),
+              name=conv_name_base + '2a')(input_tensor)
+    x = BatchNormalization(name=bn_name_base + '2a')(x, training=training)
+    x = Activation('relu')(x)
+#     x = Dropout(0.2)(x)
+
+    x = Conv1D(filters,
+               kernel_size=kernel_size,
+               strides=1,
+               padding='same',
+              kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+              bias_regularizer=regularizers.l2(1e-4),
+              activity_regularizer=regularizers.l2(1e-5),
+              name=conv_name_base + '2b')(x)
+    x = BatchNormalization(name=bn_name_base + '2b')(x, training=training)
+
+    if input_tensor.shape[2] != x.shape[2]:
+        x = layers.add([x, Lambda(lambda y: K.repeat_elements(y, rep=2, axis=2))(input_tensor)])
+    else:
+        x = layers.add([x, input_tensor])
+
+    x = BatchNormalization()(x, training=training)
+    x = Activation('relu')(x)
+#     x = Dropout(0.2)(x)
+    return x
+
+def lstm_model(opt, training=None, inputs=None):
+  if opt.mix_model==False:
+    inputs = Input(shape=[opt.input_shape, 2])
+  x = Conv1D(64,
+               kernel_size=80,
+               strides=4,
+               padding='same',
+               kernel_initializer='glorot_uniform',
+               kernel_regularizer=regularizers.l2(l=0.0001))(inputs)
+  x = BatchNormalization()(x, training=training)
+  x = Activation('relu')(x)
+  x = MaxPooling1D(pool_size=4, strides=None)(x)
+
+  for i in range(3):
+    x = identity_block(x, kernel_size=3, filters=64, stage=1, block=i, training=training)
+
+  x = MaxPooling1D(pool_size=4, strides=None)(x)
+
+  for i in range(4):
+    x = identity_block(x, kernel_size=3, filters=128, stage=2, block=i, training=training)
+
+  x = MaxPooling1D(pool_size=4, strides=None)(x)
+
+  for i in range(23):
+    x = identity_block(x, kernel_size=3, filters=256, stage=3, block=i, training=training)
+
+  x = MaxPooling1D(pool_size=4, strides=None)(x)
+
+  for i in range(3):
+    x = identity_block(x, kernel_size=3, filters=512, stage=4, block=i, training=training)
+  x = tf.keras.layers.Bidirectional(LSTM(units=256, return_sequences=False))(x)
+
+  if opt.mix_model:
+      return x
+  x = Dense(units=opt.num_classes, activation='sigmoid')(x)
+  m = Model(inputs, x)
+  return m
+
+def lstm_extracted_model(opt, training=None, inputs=None):
+  x = GlobalAveragePooling1D()(inputs) 
+  x = tf.keras.layers.Dense(256,   activation='relu',
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
-                                     activity_regularizer=regularizers.l2(1e-5))(v)
-    x2 = tf.keras.layers.Dense(128,   activation='relu',
-                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-                                     bias_regularizer=regularizers.l2(1e-4),
-                                     activity_regularizer=regularizers.l2(1e-5))(q)
-
-    q = tf.expand_dims(q, axis=-2)
-    k = tf.expand_dims(k, axis=-2)
-    v = tf.expand_dims(v, axis=-2)
-
-    q = tf.keras.layers.Dense(256,   activation='relu',
-                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-                                     bias_regularizer=regularizers.l2(1e-4),
-                                     activity_regularizer=regularizers.l2(1e-5))(q)
-    k = tf.keras.layers.Dense(256,   activation='relu',
-                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-                                     bias_regularizer=regularizers.l2(1e-4),
-                                     activity_regularizer=regularizers.l2(1e-5))(k)
-    v = tf.keras.layers.Dense(256,   activation='relu',
-                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-                                     bias_regularizer=regularizers.l2(1e-4),
-                                     activity_regularizer=regularizers.l2(1e-5))(v)
-    
-    ma  = MultiHeadAttention(head_size=num_heads, num_heads=num_heads)([q, k, v]) 
-    ma = BatchNormalization()(ma, training=training)
-    ma = Activation('relu')(ma) 
-    ma = tf.keras.layers.GRU(128, return_sequences=False)(ma) + x1 + x2
-    ma = Dropout(0.1)(ma, training=training)
-    return ma
-
-
-def mix_model(opt, cnn_1d_model, resnet_50, lstm_extracted_model, input_1D, input_2D, input_extracted, training=False):
-  out_1D = cnn_1d_model(opt, training, input_1D)
-  out_2D = resnet_50(opt)(input_2D, training=training)
-  out_extracted = lstm_extracted_model(opt, training, input_extracted)
-  
-  network_1D = Model(input_1D, out_1D, name='network_1D')
-  network_2D = Model(input_2D, out_2D, name='network_2D')
-  network_extracted = Model(input_extracted, out_extracted, name='network_extracted')
-  
-  hidden_out_1D = network_1D([input_1D])
-  hidden_out_2D = network_2D([input_2D])
-  hidden_out_extracted = network_extracted([input_extracted])
-  
-  merged_value_0 = concatenate((hidden_out_1D, hidden_out_2D, hidden_out_extracted))
-  merged_value_1 = TransformerLayer(hidden_out_1D, hidden_out_2D, hidden_out_extracted, 8, training)
-    
-  Condition = Dense(3, 
-                    activation='softmax', 
-                    name='Condition')(merged_value_0)
-  RUL = Dense(1, 
-              activation='sigmoid', 
-              name='RUL')(merged_value_1)
-  return Condition, RUL
+                                     activity_regularizer=regularizers.l2(1e-5))(x)
+  return x
