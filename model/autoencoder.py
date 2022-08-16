86e3b46c040326e96054b1805ab43169c53f9004
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-16 08:56:25 +0700

    Update autoencoder.py

--- model/autoencoder.py
+++ model/autoencoder.py
@@ -2,12 +2,12 @@ from tensorflow.keras.layers import Input, Dropout, Dense, LSTM, TimeDistributed
 
 def autoencoder_model(X):
     inputs = Input(shape=(X.shape[1], X.shape[2]))
-    L1 = LSTM(1024, activation='relu', return_sequences=True, 
-              kernel_regularizer=regularizers.l2(0.00))(inputs)
-    L2 = LSTM(512, activation='relu', return_sequences=False)(L1)
+    L1 = LSTM(1024, activation='tanh', return_sequences=True, 
+              kernel_regularizer=tf.keras.regularizers.l2(0.00))(inputs)
+    L2 = LSTM(32, activation='tanh', return_sequences=False)(L1)
     L3 = RepeatVector(X.shape[1])(L2)
-    L4 = LSTM(512, activation='relu', return_sequences=True)(L3)
-    L5 = LSTM(1024, activation='relu', return_sequences=True)(L4)
+    L4 = LSTM(32, activation='tanh', return_sequences=True)(L3)
+    L5 = LSTM(1024, activation='tanh', return_sequences=True)(L4)
     output = TimeDistributed(Dense(X.shape[2]))(L5)    
     model = Model(inputs=inputs, outputs=output)
     return model
