fa9f79fa181cc427cc1b0d182597d4ce5220a6f9
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-17 19:21:11 +0700

    Update MIX_1D_2D.py

--- model/MIX_1D_2D.py
+++ model/MIX_1D_2D.py
@@ -6,37 +6,22 @@ from keras import layers, regularizers
 import keras.backend as K
 
 def TransformerLayer(q, v, k, num_heads=4, training=None):
-    # Transformer layer https://arxiv.org/abs/2010.11929 (LayerNorm layers removed for better performance)
 
-    # x1 = tf.keras.layers.Dense(128,   activation='relu',
-    #                                  kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-    #                                  bias_regularizer=regularizers.l2(1e-4),
-    #                                  activity_regularizer=regularizers.l2(1e-5))(v)
-    # x2 = tf.keras.layers.Dense(128,   activation='relu',
-    #                                  kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-    #                                  bias_regularizer=regularizers.l2(1e-4),
-    #                                  activity_regularizer=regularizers.l2(1e-5))(q)
-
-    q = tf.expand_dims(q, axis=-2)
-    k = tf.expand_dims(k, axis=-2)
-    v = tf.expand_dims(v, axis=-2)
-
-    q = tf.keras.layers.Dense(256,   activation='relu',
+    q = tf.keras.layers.Dense(128,   activation='relu',
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(q)
-    k = tf.keras.layers.Dense(256,   activation='relu',
+    k = tf.keras.layers.Dense(128,   activation='relu',
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(k)
-    v = tf.keras.layers.Dense(256,   activation='relu',
+    v = tf.keras.layers.Dense(128,   activation='relu',
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(v)
     
     ma  = MultiHeadAttention(head_size=num_heads, num_heads=num_heads)([q, k, v]) 
     ma = BatchNormalization()(ma, training=training)
-    ma = tf.keras.layers.GRU(units=128, return_sequences=False, activation='relu')(ma) 
     ma = Dropout(0.1)(ma, training=training)
     return ma
 
@@ -69,13 +54,13 @@ def mix_model(opt, cnn_1d_model, resnet_50, lstm_extracted_model, input_1D, inpu
   hidden_out_2D = network_2D([input_2D])
   hidden_out_extracted = network_extracted([input_extracted])
   
-  merged_value_0 = fully_concatenate(hidden_out_1D, hidden_out_2D, hidden_out_extracted)
   merged_value_1 = TransformerLayer(hidden_out_1D, hidden_out_2D, hidden_out_extracted, 8, training)
+  merged_value_0 = fully_concatenate(hidden_out_1D, hidden_out_2D, merged_value_1)
     
   Condition = Dense(3, 
                     activation='softmax', 
                     name='Condition')(merged_value_0)
   RUL = Dense(1, 
               activation='sigmoid', 
-              name='RUL')(merged_value_1)
+              name='RUL')(merged_value_0)
   return Condition, RUL
