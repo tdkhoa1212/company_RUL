12bcd86896c3277c47d1a68fde28080de8d76ffc
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-22 09:20:29 +0700

    Update MIX_1D_2D.py

--- model/MIX_1D_2D.py
+++ model/MIX_1D_2D.py
@@ -1,10 +1,10 @@
 from tensorflow.keras.layers import Conv1D, Activation, Dense, concatenate, BatchNormalization, GlobalAveragePooling1D, Input, MaxPooling1D, Lambda, GlobalAveragePooling2D, ReLU, MaxPooling2D, Flatten, Dropout, LSTM
 import tensorflow as tf
 from keras.models import Model
-from tensorflow_addons.layers import MultiHeadAttention
 from keras import layers, regularizers
 import keras.backend as K
 
+from tensorflow_addons.layers import MultiHeadAttention
 # def TransformerLayer(x, num_heads=4, training=None):
 #     x = tf.keras.layers.Dense(56,   activation='relu',
 #                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
@@ -27,28 +27,28 @@ import keras.backend as K
 #     return ma
 
 def fully_concatenate(hidden_out_1D, hidden_out_2D, hidden_out_extracted, training):
-#     hidden_out_1D = tf.keras.layers.Dense(1024,   activation='relu',
-#                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-#                                      bias_regularizer=regularizers.l2(1e-4),
-#                                      activity_regularizer=regularizers.l2(1e-5))(hidden_out_1D) 
-#     hidden_out_2D = tf.keras.layers.Dense(512,   activation='relu',
-#                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-#                                      bias_regularizer=regularizers.l2(1e-4),
-#                                      activity_regularizer=regularizers.l2(1e-5))(hidden_out_2D) 
-#     hidden_out_extracted = tf.keras.layers.Dense(64,   activation='relu',
-#                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
-#                                      bias_regularizer=regularizers.l2(1e-4),
-#                                      activity_regularizer=regularizers.l2(1e-5))(hidden_out_extracted) 
-#     hidden_out_1D = Dropout(0.1)(hidden_out_1D, training=training)
-#     hidden_out_2D = Dropout(0.1)(hidden_out_2D, training=training)
-#     hidden_out_extracted = Dropout(0.1)(hidden_out_extracted, training=training)
-    
-    all_ = concatenate((hidden_out_1D, hidden_out_2D, hidden_out_extracted))
-    all_ = tf.keras.layers.Dense(1024,   activation='relu',
+    hidden_out_1D = tf.keras.layers.Dense(1024,   activation='relu',
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
-                                     activity_regularizer=regularizers.l2(1e-5))(all_) 
-    all_ = Dropout(0.1)(all_, training=training)
+                                     activity_regularizer=regularizers.l2(1e-5))(hidden_out_1D) 
+    hidden_out_2D = tf.keras.layers.Dense(512,   activation='relu',
+                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+                                     bias_regularizer=regularizers.l2(1e-4),
+                                     activity_regularizer=regularizers.l2(1e-5))(hidden_out_2D) 
+    hidden_out_extracted = tf.keras.layers.Dense(64,   activation='relu',
+                                     kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+                                     bias_regularizer=regularizers.l2(1e-4),
+                                     activity_regularizer=regularizers.l2(1e-5))(hidden_out_extracted) 
+    hidden_out_1D = Dropout(0.1)(hidden_out_1D, training=training)
+    hidden_out_2D = Dropout(0.1)(hidden_out_2D, training=training)
+    hidden_out_extracted = Dropout(0.1)(hidden_out_extracted, training=training)
+    
+    all_ = concatenate((hidden_out_1D, hidden_out_2D, hidden_out_extracted))
+#     all_ = tf.keras.layers.Dense(1024,   activation='relu',
+#                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+#                                      bias_regularizer=regularizers.l2(1e-4),
+#                                      activity_regularizer=regularizers.l2(1e-5))(all_) 
+#     all_ = Dropout(0.1)(all_, training=training)
     return all_
 
 def mix_model(opt, cnn_1d_model, resnet_50, lstm_extracted_model, input_1D, input_2D, input_extracted, training=False):
