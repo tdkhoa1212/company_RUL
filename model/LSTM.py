099a285fd8fe5b709e751bb9ccc7f5f8709d16c7
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-25 18:29:08 +0700

    Update LSTM.py

--- model/LSTM.py
+++ model/LSTM.py
@@ -10,10 +10,12 @@ from tensorflow_addons.layers import MultiHeadAttention
 
 '''
 def TransformerLayer(x, c, num_heads=4, training=None):
+    a = x
     x = tf.keras.layers.Dense(c,   activation='relu',
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(x)
+    x = Dropout(0.1)(x, training=training)
     ma  = MultiHeadAttention(head_size=num_heads, num_heads=num_heads)([x, x, x]) 
     ma = BatchNormalization()(ma, training=training)
     ma = tf.keras.layers.Dense(c,   activation='relu',
@@ -26,6 +28,7 @@ def TransformerLayer(x, c, num_heads=4, training=None):
                                      activity_regularizer=regularizers.l2(1e-5))(ma) 
     ma = Dropout(0.1)(ma, training=training)
     ma = tf.keras.layers.Bidirectional(LSTM(units=c, return_sequences=False, activation='relu'))(ma)
+    ma = Dropout(0.1)(ma, training=training)
     return ma
 '''
 
