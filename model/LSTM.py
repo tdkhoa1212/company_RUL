0f4a82dc0baaae9841c79f70f1438f8850e4ea09
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-27 05:48:24 +0700

    Update LSTM.py

--- model/LSTM.py
+++ model/LSTM.py
@@ -36,20 +36,20 @@ def TransformerLayer(x, c, num_heads=16, training=None):
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(x)
-    x = Dropout(0.1)(x, training=training)
+    x = Dropout(0.2)(x, training=training)
     ma  = MultiHeadAttention(head_size=num_heads, num_heads=num_heads)([x, x, x]) 
     ma = tf.keras.layers.Dense(c,   activation='relu',
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(ma) 
-    ma = Dropout(0.1)(ma, training=training)
+    ma = Dropout(0.2)(ma, training=training)
     ma = tf.keras.layers.Dense(c,  activation='relu',
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(ma) 
-    ma = Dropout(0.1)(ma, training=training)
-    ma = tf.keras.layers.Bidirectional(LSTM(units=c, return_sequences=False, activation='relu', recurrent_dropout=0.1))(ma)
-    ma = Dropout(0.1)(ma, training=training)
+    ma = Dropout(0.2)(ma, training=training)
+    ma = tf.keras.layers.Bidirectional(LSTM(units=c, return_sequences=False, activation='relu', recurrent_dropout=0.2))(ma)
+    ma = Dropout(0.2)(ma, training=training)
     return ma
 
 def identity_block(input_tensor, kernel_size, filters, stage, block, training):
