1f1d7030ed25f23081dae5564cb09bb04b401d2a
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-29 06:45:26 +0700

    Update MIX_1D_2D.py

--- model/MIX_1D_2D.py
+++ model/MIX_1D_2D.py
@@ -20,8 +20,8 @@ def reshape(x):
 def mix_model(opt, cnn_1d_model, resnet_50, lstm_extracted_model, input_1D, input_2D, input_extracted, training=False):
   out_1D = cnn_1d_model(opt, training, input_1D)
 #   out_2D = resnet_50(opt)(input_2D, training=training)
-  base_model_2D = tf.keras.applications.EfficientNetV2B0(include_top=False,
-                                                         input_shape=(128, 128, 1),
+  base_model_2D = tf.keras.applications.EfficientNetV2B3(include_top=False,
+                                                         input_shape=(128, 128, 2),
                                                          weights=None)
   out_2D = base_model_2D(input_2D, training=training)
   out_extracted = lstm_extracted_model(opt, training, input_extracted)
