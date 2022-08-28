c06d8830f327d47ec528000cf444864e7efc4a9f
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-28 20:34:48 +0700

    Update autoencoder.py

--- model/autoencoder.py
+++ model/autoencoder.py
@@ -11,12 +11,11 @@ def autoencoder_model(type_):
       inputs = Input(shape=(2, 32768))
       x1 = 2
       x2 = 32768
-    L1 = LSTM(1024, activation='tanh', return_sequences=True, 
-              kernel_regularizer=tf.keras.regularizers.l2(0.00))(inputs)
-    L2 = LSTM(32, activation='tanh', return_sequences=False)(L1)
+    L1 = LSTM(1024, activation='tanh', return_sequences=True, kernel_regularizer=tf.keras.regularizers.l2(0.00), dropout=0.1)(inputs)
+    L2 = LSTM(32, activation='tanh', return_sequences=False, kernel_regularizer=tf.keras.regularizers.l2(0.00), dropout=0.1)(L1)
     L3 = RepeatVector(x1)(L2)
-    L4 = LSTM(32, activation='tanh', return_sequences=True)(L3)
-    L5 = LSTM(1024, activation='tanh', return_sequences=True)(L4)
+    L4 = LSTM(32, activation='tanh', return_sequences=True, kernel_regularizer=tf.keras.regularizers.l2(0.00), dropout=0.1)(L3)
+    L5 = LSTM(1024, activation='tanh', return_sequences=True, kernel_regularizer=tf.keras.regularizers.l2(0.00), dropout=0.1)(L4)
     output = TimeDistributed(Dense(x2))(L5)    
     model = Model(inputs=inputs, outputs=output)
     return model
