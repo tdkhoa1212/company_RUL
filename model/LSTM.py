e7ae28f9d2f4cf45465ea7aa6186fac539e3ce2c
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-08-29 18:15:40 +0700

    Update LSTM.py

--- model/LSTM.py
+++ model/LSTM.py
@@ -32,24 +32,21 @@ def TransformerLayer(x, c, num_heads=16, training=None):
 '''
 
 def TransformerLayer(x, c, num_heads=16, training=None):
-    a = x
     x = tf.keras.layers.Dense(c,   activation='relu',
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(x)
     x = Dropout(0.2)(x, training=training)
-    ma  = MultiHeadAttention(head_size=num_heads, num_heads=num_heads)([x, x, x]) + a
-    ma = BatchNormalization()(ma, training=training)
-    ma = Dropout(0.2)(ma, training=training)
+    ma  = MultiHeadAttention(head_size=num_heads, num_heads=num_heads)([x, x, x]) 
     ma = tf.keras.layers.Dense(c,   activation='relu',
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(ma) 
+    ma = Dropout(0.2)(ma, training=training)
     ma = tf.keras.layers.Dense(c,  activation='relu',
                                      kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
                                      bias_regularizer=regularizers.l2(1e-4),
-                                     activity_regularizer=regularizers.l2(1e-5))(ma) + a
-    ma = BatchNormalization()(ma, training=training)
+                                     activity_regularizer=regularizers.l2(1e-5))(ma) 
     ma = Dropout(0.2)(ma, training=training)
     ma = tf.keras.layers.Bidirectional(LSTM(units=c, return_sequences=False, activation='relu', recurrent_dropout=0.2))(ma)
     ma = Dropout(0.2)(ma, training=training)
@@ -100,17 +97,20 @@ def lstm_model(opt, training=None, inputs=None):
                kernel_regularizer=regularizers.l2(l=0.0001))(inputs)
   x = BatchNormalization()(x, training=training)
   x = Activation('relu')(x)
-  x = MaxPooling1D(pool_size=4, strides=None)(x)
+#   x = MaxPooling1D(pool_size=4, strides=None)(x)
+  x = AveragePooling1D(pool_size=4, strides=None)(x)
 
   for i in range(3):
     x = identity_block(x, kernel_size=3, filters=64, stage=1, block=i, training=training)
 
-  x = MaxPooling1D(pool_size=4, strides=None)(x)
+#   x = MaxPooling1D(pool_size=4, strides=None)(x)
+  x = AveragePooling1D(pool_size=4, strides=None)(x)
 
   for i in range(4):
     x = identity_block(x, kernel_size=3, filters=128, stage=2, block=i, training=training)
 
-  x = MaxPooling1D(pool_size=4, strides=None)(x)
+#   x = MaxPooling1D(pool_size=4, strides=None)(x)
+  x = AveragePooling1D(pool_size=4, strides=None)(x)
 
   for i in range(23):
     x = identity_block(x, kernel_size=3, filters=256, stage=3, block=i, training=training)
@@ -137,6 +137,14 @@ def lstm_extracted_model(opt, training=None, inputs=None):
   x = Activation('relu')(x)
   x = AveragePooling1D(pool_size=2)(x)
 
+  x = Conv1D(56,
+               kernel_size=4,
+               strides=1,
+               padding='same',
+               kernel_initializer='glorot_uniform',
+               kernel_regularizer=regularizers.l2(l=0.0001))(x)
+  x = BatchNormalization()(x, training=training)
+  x = Activation('relu')(x)
   x = Conv1D(56,
                kernel_size=4,
                strides=1,
