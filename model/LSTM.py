2cf883e31ce834f45c460c8479c356b0684dd867
tdkhoa1212 <69751665+tdkhoa1212@users.noreply.github.com>
2022-09-11 11:35:54 +0700

    Update LSTM.py

--- model/LSTM.py
+++ model/LSTM.py
@@ -48,8 +48,8 @@ def TransformerLayer(x, c, num_heads=16, training=None):
                                      bias_regularizer=regularizers.l2(1e-4),
                                      activity_regularizer=regularizers.l2(1e-5))(ma) 
     ma = Dropout(0.2)(ma, training=training)
-    ma = tf.keras.layers.Bidirectional(LSTM(units=c, return_sequences=True, activation='relu', recurrent_dropout=0.2))(ma)
-#     ma = Dropout(0.2)(ma, training=training)
+    ma = tf.keras.layers.Bidirectional(LSTM(units=c, return_sequences=False, activation='relu', recurrent_dropout=0.2))(ma)
+    ma = Dropout(0.2)(ma, training=training)
     return ma
 
 def identity_block(input_tensor, kernel_size, filters, stage, block, training):
