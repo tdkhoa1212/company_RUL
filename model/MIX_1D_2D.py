98a56d2183854a3100d1183db6548b9ef4b8538a
tdkhoa1212 <tdkhoa1212@gmail.com>
2022-12-05 21:58:51 +0700

    a

--- model/MIX_1D_2D.py
+++ model/MIX_1D_2D.py
@@ -47,7 +47,7 @@ def reshape(x):
     out = Reshape((x.shape[-2]*x.shape[-3], x.shape[-1]))(x)
     return out
 
-def mix_model(opt, cnn_1d_model, resnet_50, lstm_extracted_model, input_1D, input_2D, input_extracted, training=False):
+def mix_model_XJTU(opt, cnn_1d_model, resnet_50, lstm_extracted_model, input_1D, input_2D, input_extracted, training=False):
   out_1D = cnn_1d_model(opt, training, input_1D)
   out_2D = resnet_50(opt)(input_2D, training=training)
 
@@ -82,13 +82,53 @@ def mix_model(opt, cnn_1d_model, resnet_50, lstm_extracted_model, input_1D, inpu
   con_hidden_out_2D = GlobalAveragePooling2D()(hidden_out_2D)
   con_hidden_out_extracted = GlobalAveragePooling1D()(hidden_out_extracted)
   
-  # fully connected layers---------------------------------------------------
+  # Fully connected layers---------------------------------------------------
   merged_value_0 = fully_concatenate(rul_hidden_out_1D, rul_hidden_out_2D, rul_hidden_out_extracted, training=training, fully=False)
   merged_value_1 = fully_concatenate(con_hidden_out_1D, con_hidden_out_2D, con_hidden_out_extracted, training=training, fully=False)
     
+  # Final output layers---------------------------------------------------
   Condition = Dense(3, 
                     activation='softmax', 
                     name='Condition')(merged_value_1)
+  RUL = Dense(1, 
+              activation='sigmoid', 
+              name='RUL')(merged_value_0)
+  return Condition, RUL
+
+def mix_model_PHM(opt, cnn_1d_model, resnet_50, lstm_extracted_model, input_1D, input_2D, input_extracted, training=False):
+  out_1D = cnn_1d_model(opt, training, input_1D)
+  out_2D = resnet_50(opt)(input_2D, training=training)
+
+  ##################### https://keras.io/api/applications/ #######################################
+#   base_model_2D = tf.keras.applications.EfficientNetV2B3(include_top=False,
+#                                                          input_shape=(128, 128, 2),
+#                                                          weights=None)
+#   out_2D = base_model_2D(input_2D, training=training)
+
+  out_extracted = lstm_extracted_model(opt, training, input_extracted)
+  
+  # 1D branch-------------------------------------------
+  network_1D = Model(input_1D, out_1D, name='network_1D')
+  # 2D branch-------------------------------------------
+  network_2D = Model(input_2D, out_2D, name='network_2D')
+  # Extracted branch-------------------------------------------
+  network_extracted = Model(input_extracted, out_extracted, name='network_extracted')
+  
+  # Hidden layers----------------------------------------------------------
+  hidden_out_1D = network_1D([input_1D])
+  hidden_out_2D = network_2D([input_2D])
+  hidden_out_extracted = network_extracted([input_extracted])
+  
+  # Transformer layers-----------------------------------------------------
+  rul_hidden_out_1D = TransformerLayer(hidden_out_1D, hidden_out_1D.shape[-1], training=training)
+  rul_hidden_out_2D = reshape(hidden_out_2D)
+  rul_hidden_out_2D = TransformerLayer(rul_hidden_out_2D, rul_hidden_out_2D.shape[-1], training=training)
+  rul_hidden_out_extracted = TransformerLayer(hidden_out_extracted, hidden_out_extracted.shape[-1], training=training)
+  
+  # Fully connected layers---------------------------------------------------
+  merged_value_0 = fully_concatenate(rul_hidden_out_1D, rul_hidden_out_2D, rul_hidden_out_extracted, training=training, fully=False)
+    
+  # Final output layer---------------------------------------------------
   RUL = Dense(1, 
               activation='sigmoid', 
               name='RUL')(merged_value_0)
--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from model.autoencoder import autoencoder_model
 from model.cnn import cnn_1d_model
-from model.MIX_1D_2D import mix_model
+from model.MIX_1D_2D import mix_model_PHM, mix_model_XJTU
 from model.resnet import resnet_101, resnet_50
 from model.LSTM import lstm_extracted_model, lstm_model
 from utils.tools import to_onehot
@@ -53,42 +53,73 @@ def parse_opt(known=False):
     opt = parser.parse_known_args()[0] if known else parser.parse_args()
     return opt
 
-def main(opt, train_1D, train_2D, train_extract, train_label_RUL, train_label_Con, test_1D, test_2D, test_extract, test_label_RUL, test_label_Con):  
+# Train and test for PHM data ############################################################################################
+def main_PHM(opt, train_1D, train_2D, train_extract, train_label_RUL, test_1D, test_2D, test_extract, test_label_RUL):  
+  train_label_Con = to_onehot(train_label_Con)
+  val_2D, val_1D, val_extract, val_label_RUL = test_2D, test_1D, test_extract, test_label_RUL
+  val_data = [val_1D, val_2D, val_extract]
+  val_label = val_label_RUL
+
+  input_extracted = Input((14, 2), name='Extracted_LSTM_input')
+  input_1D = Input((opt.input_shape, 2), name='LSTM_CNN1D_input')
+  input_2D = Input((128, 128, 2), name='CNN_input')
+  Condition, RUL = mix_model(opt, lstm_model, resnet_101, lstm_extracted_model, input_1D, input_2D, input_extracted, True)
+  network = Model(inputs=[input_1D, input_2D, input_extracted], outputs=[Condition, RUL])
+
+  # get three types of different forms from original data-------------------------------
+  train_data = [train_1D, train_2D, train_extract]
+  train_label = train_label_RUL
+  test_data = [test_1D, test_2D, test_extract]
+  test_label = test_label_RUL
+  
+  weight_path = os.path.join(opt.save_dir, f'model_{opt.condition}_{opt.type}')
+  if opt.load_weight:
+    if os.path.exists(weight_path):
+      print(f'\nLoad weight: {weight_path}\n')
+      network.load_weights(weight_path)
+  callback = tf.keras.callbacks.EarlyStopping(monitor='val_acc', patience=1)
+  network.compile(optimizer=tf.keras.optimizers.RMSprop(1e-4),
+                  loss=['categorical_crossentropy', tf.keras.losses.MeanSquaredLogarithmicError()], 
+                  metrics=['acc', 'mae', tfa.metrics.RSquare(), tf.keras.metrics.RootMeanSquaredError()], 
+                  loss_weights=[0.01, 1],
+#                   run_eagerly=True
+                    ) # https://keras.io/api/losses/ 
+  network.summary()
+
+  # dataset_train = tf.data.Dataset.from_tensor_slices((train_data , train_label)).batch(opt.batch_size)
+  tf.debugging.set_log_device_placement(True)
+
+  history = network.fit(train_data , train_label_RUL,
+                        epochs     = opt.epochs,
+                        batch_size = opt.batch_size,
+                        validation_data = (val_data, val_label_RUL))
+  network.save(weight_path)
+  _, _, _, Condition_acc, _, _, _, _, RUL_mae, RUL_r_square, RUL_mean_squared_error = network.evaluate(test_data, test_label_RUL, verbose=0)
+  Condition_acc = round(Condition_acc*100, 4)
+  RUL_mae = round(RUL_mae, 4)
+  RUL_r_square = round(RUL_r_square, 4)
+  RUL_mean_squared_error = round(RUL_mean_squared_error, 4)
+  print(f'\n----------Score in test set: \n Condition acc: {Condition_acc}, mae: {RUL_mae}, r2: {RUL_r_square}, rmse: {RUL_mean_squared_error}\n' )
+
+# Train and test for XJTU data ############################################################################################
+def main_XJTU(opt, train_1D, train_2D, train_extract, train_label_RUL, train_label_Con, test_1D, test_2D, test_extract, test_label_RUL, test_label_Con):  
   train_label_Con = to_onehot(train_label_Con)
   test_label_Con  = to_onehot(test_label_Con)
   val_2D, val_1D, val_extract, val_label_Con, val_label_RUL = test_2D, test_1D, test_extract, test_label_Con, test_label_RUL
   val_data = [val_1D, val_2D, val_extract]
   val_label = [val_label_Con, val_label_RUL]
 
-  if opt.model == 'dnn':
-    train_data = [train_2D[:, :, 0], train_2D[:, :, 1]]
-    val_data, val_label = [test_2D[:100][:, :, 0], test_2D[:100][:, :, 1]], test_label_RUL[:100]
-    test_data = [test_2D[:, :, 0], test_data[:, :, 1]]
-    network = dnn_model(opt)
-  if opt.model == 'cnn_1d':
-    network = cnn_1d_model(opt, training=True)
-  if opt.model == 'resnet_cnn_2d':
-    inputs = Input(shape=[128, 128, 2])
-    output = resnet_50(opt)(inputs, training=True)
-    network = Model(inputs, output)
-  if opt.model == 'cnn_2d':
-    network = cnn_2d_model(opt, [128, 128, 2])
-  if opt.model == 'autoencoder':
-    network = autoencoder_model(train_data)
-  if opt.model == 'lstm':
-    network = lstm_model(opt, training=True)
-  if opt.mix_model:
-    input_extracted = Input((14, 2), name='Extracted_LSTM_input')
-    input_1D = Input((opt.input_shape, 2), name='LSTM_CNN1D_input')
-    input_2D = Input((128, 128, 2), name='CNN_input')
-    Condition, RUL = mix_model(opt, lstm_model, resnet_101, lstm_extracted_model, input_1D, input_2D, input_extracted, True)
-    network = Model(inputs=[input_1D, input_2D, input_extracted], outputs=[Condition, RUL])
+  input_extracted = Input((14, 2), name='Extracted_LSTM_input')
+  input_1D = Input((opt.input_shape, 2), name='LSTM_CNN1D_input')
+  input_2D = Input((128, 128, 2), name='CNN_input')
+  Condition, RUL = mix_model(opt, lstm_model, resnet_101, lstm_extracted_model, input_1D, input_2D, input_extracted, True)
+  network = Model(inputs=[input_1D, input_2D, input_extracted], outputs=[Condition, RUL])
 
-    # get three types of different forms from original data-------------------------------
-    train_data = [train_1D, train_2D, train_extract]
-    train_label = [train_label_Con, train_label_RUL]
-    test_data = [test_1D, test_2D, test_extract]
-    test_label = [test_label_Con, test_label_RUL]
+  # get three types of different forms from original data-------------------------------
+  train_data = [train_1D, train_2D, train_extract]
+  train_label = [train_label_Con, train_label_RUL]
+  test_data = [test_1D, test_2D, test_extract]
+  test_label = [test_label_Con, test_label_RUL]
   
   weight_path = os.path.join(opt.save_dir, f'model_{opt.condition}_{opt.type}')
   if opt.load_weight:
@@ -124,8 +155,9 @@ if __name__ == '__main__':
   # start_save_data(opt)
   if opt.type == 'PHM':
     from utils.load_PHM_data import train_1D, train_2D, train_extract, train_label_RUL,\
-                                    test_1D, test_2D, test_extract, test_label_Con, test_label_RUL
+                                    test_1D, test_2D, test_extract, test_label_RUL
+    main_PHM(opt, train_1D, train_2D, train_extract, train_label_RUL, test_1D, test_2D, test_extract, test_label_RUL)
   else:
     from utils.load_XJTU_data import train_1D, train_2D, train_extract, train_label_Con, train_label_RUL,\
                                      test_1D, test_2D, test_extract, test_label_Con, test_label_RUL
-  main(opt, train_1D, train_2D, train_extract, train_label_RUL, train_label_Con, test_1D, test_2D, test_extract, test_label_RUL, test_label_Con)
+    main_XJTU(opt, train_1D, train_2D, train_extract, train_label_RUL, train_label_Con, test_1D, test_2D, test_extract, test_label_RUL, test_label_Con)
