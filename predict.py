169110240fd79aa05d228cef14bc026963793fd4
tdkhoa1212 <tdkhoa1212@gmail.com>
2022-12-07 00:39:18 +0700

    a

--- predict.py
+++ predict.py
@@ -63,22 +63,26 @@ def main(opt):
     Condition, RUL = Predict([test_1D, test_2D, test_extract])
 
   if opt.type == 'PHM':
-    r2, mae_, mse_ = all_matric_PHM(test_label_RUL, RUL, test_label_Con, Condition, opt)
+    r2, mae_, mse_ = all_matric_PHM(test_label_RUL, RUL)
   else:
     Condition = back_onehot(Condition)
-    r2, mae_, mse_, acc = all_matric_XJTU(test_label_RUL, RUL, test_label_Con, Condition, opt)
+    r2, mae_, mse_, acc = all_matric_XJTU(test_label_RUL, RUL, test_label_Con, Condition)
     acc = round(acc*100, 4)
 
   mae_ = round(mae_, 4)
   rmse_ = round(mse_, 4)
   r2 = round(r2, 4)
-  print(f'\n-----{name}:      R2: {r2}, MAE: {mae_}, RMSE: {rmse_}, Acc: {acc}-----')
+
+  if opt.type == 'PHM':
+    print(f'\n-----{name}:      R2: {r2}, MAE: {mae_}, RMSE: {rmse_}-----')
+  else:
+    print(f'\n-----{name}:      R2: {r2}, MAE: {mae_}, RMSE: {rmse_}, Acc: {acc}-----')
 
   # Simulating the graphs --------------------------------------------------------
   plt.plot(test_label_RUL, c='b')
   plt.plot(RUL, c='r')
-  plt.title(f'{name}: combination prediction.')
-  plt.savefig(f'{name}.png')
+  plt.title(opt.type + f' - {name}.')
+  plt.savefig(opt.save_dir + f'/{name}.png')
   plt.close()
 
     
