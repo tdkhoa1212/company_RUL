0419cee65c9e67941cf1c400a77a6fea3a27640b
tdkhoa1212 <tdkhoa1212@gmail.com>
2022-12-07 19:15:25 +0700

    a

--- utils/load_PHM_data.py
+++ utils/load_PHM_data.py
@@ -1,4 +1,5 @@
 import os
+from os.path import join, exists
 import numpy as np
 from train import parse_opt
 from utils.tools import  save_df, convert_to_image, getting_data
@@ -7,9 +8,9 @@ opt = parse_opt()
 np.random.seed(1234)
 
 # Link of original data ==================================================================================
-train_dir = os.path.join(opt.main_dir_colab, '/PHM_data/Learning_set/')
-test_dir = os.path.join(opt.main_dir_colab, '/PHM_data/Test_set/')
-saved_dir = os.path.join(opt.main_dir_colab, '/PHM_data/saved_data/')
+train_dir = join(opt.main_dir_colab, '/PHM_data/Learning_set/')
+test_dir = join(opt.main_dir_colab, '/PHM_data/Test_set/')
+saved_dir = join(opt.main_dir_colab, '/PHM_data/saved_data/')
 
 # FPT points of bearing sets ==================================================================================
 FPT = {'Bearing1_1': 1314,
@@ -21,34 +22,34 @@ FPT = {'Bearing1_1': 1314,
        'Bearing1_7': 2210}
 
 # Saving the converted data ==================================================================================
-if os.path.exists(saved_dir + 'Bearing1_1_' + '1d') == False:
+if exists(saved_dir + 'Bearing1_1_' + '1d') == False:
   for type_data in opt.data_type:
     # Converting data-------------------------------------------------------------------------
     print(f'\n Saving data in {opt.type} data set'+'-'*100)
-    Bearing1_1 = convert_to_image(os.path.join(train_dir, 'Bearing1_1'), opt, type_data, FPT['Bearing1_1'], 'PHM')
-    Bearing1_2 = convert_to_image(os.path.join(train_dir, 'Bearing1_2'), opt, type_data, FPT['Bearing1_2'], 'PHM')
-    Bearing1_3 = convert_to_image(os.path.join(test_dir, 'Bearing1_3'),  opt, type_data, FPT['Bearing1_3'], 'PHM')
-    Bearing1_4 = convert_to_image(os.path.join(test_dir, 'Bearing1_4'),  opt, type_data, FPT['Bearing1_4'], 'PHM')
-    Bearing1_5 = convert_to_image(os.path.join(test_dir, 'Bearing1_5'),  opt, type_data, FPT['Bearing1_5'], 'PHM')
-    Bearing1_6 = convert_to_image(os.path.join(test_dir, 'Bearing1_6'),  opt, type_data, FPT['Bearing1_6'], 'PHM')
-    Bearing1_7 = convert_to_image(os.path.join(test_dir, 'Bearing1_7'),  opt, type_data, FPT['Bearing1_7'], 'PHM')
+    Bearing1_1 = convert_to_image(join(train_dir, 'Bearing1_1'), opt, type_data, FPT['Bearing1_1'], 'PHM')
+    Bearing1_2 = convert_to_image(join(train_dir, 'Bearing1_2'), opt, type_data, FPT['Bearing1_2'], 'PHM')
+    Bearing1_3 = convert_to_image(join(test_dir, 'Bearing1_3'),  opt, type_data, FPT['Bearing1_3'], 'PHM')
+    Bearing1_4 = convert_to_image(join(test_dir, 'Bearing1_4'),  opt, type_data, FPT['Bearing1_4'], 'PHM')
+    Bearing1_5 = convert_to_image(join(test_dir, 'Bearing1_5'),  opt, type_data, FPT['Bearing1_5'], 'PHM')
+    Bearing1_6 = convert_to_image(join(test_dir, 'Bearing1_6'),  opt, type_data, FPT['Bearing1_6'], 'PHM')
+    Bearing1_7 = convert_to_image(join(test_dir, 'Bearing1_7'),  opt, type_data, FPT['Bearing1_7'], 'PHM')
     
     # Save data and labels in different types------------------------------------------------
-    save_df(os.path.join(saved_dir, 'Bearing1_1_data' + type_data), Bearing1_1['x'])
-    save_df(os.path.join(saved_dir, 'Bearing1_2_data' + type_data), Bearing1_2['x'])
-    save_df(os.path.join(saved_dir, 'Bearing1_3_data' + type_data), Bearing1_3['x'])
-    save_df(os.path.join(saved_dir, 'Bearing1_4_data' + type_data), Bearing1_4['x'])
-    save_df(os.path.join(saved_dir, 'Bearing1_5_data' + type_data), Bearing1_5['x'])
-    save_df(os.path.join(saved_dir, 'Bearing1_6_data' + type_data), Bearing1_6['x'])
-    save_df(os.path.join(saved_dir, 'Bearing1_7_data' + type_data), Bearing1_7['x'])
+    save_df(join(saved_dir, 'Bearing1_1_data' + type_data), Bearing1_1['x'])
+    save_df(join(saved_dir, 'Bearing1_2_data' + type_data), Bearing1_2['x'])
+    save_df(join(saved_dir, 'Bearing1_3_data' + type_data), Bearing1_3['x'])
+    save_df(join(saved_dir, 'Bearing1_4_data' + type_data), Bearing1_4['x'])
+    save_df(join(saved_dir, 'Bearing1_5_data' + type_data), Bearing1_5['x'])
+    save_df(join(saved_dir, 'Bearing1_6_data' + type_data), Bearing1_6['x'])
+    save_df(join(saved_dir, 'Bearing1_7_data' + type_data), Bearing1_7['x'])
 
-    save_df(os.path.join(saved_dir, 'Bearing1_1_label_RUL'), Bearing1_1['y'])
-    save_df(os.path.join(saved_dir, 'Bearing1_2_label_RUL'), Bearing1_2['y'])
-    save_df(os.path.join(saved_dir, 'Bearing1_3_label_RUL'), Bearing1_3['y'])
-    save_df(os.path.join(saved_dir, 'Bearing1_4_label_RUL'), Bearing1_4['y'])
-    save_df(os.path.join(saved_dir, 'Bearing1_5_label_RUL'), Bearing1_5['y'])
-    save_df(os.path.join(saved_dir, 'Bearing1_6_label_RUL'), Bearing1_6['y'])
-    save_df(os.path.join(saved_dir, 'Bearing1_7_label_RUL'), Bearing1_7['y'])
+    save_df(join(saved_dir, 'Bearing1_1_label_RUL'), Bearing1_1['y'])
+    save_df(join(saved_dir, 'Bearing1_2_label_RUL'), Bearing1_2['y'])
+    save_df(join(saved_dir, 'Bearing1_3_label_RUL'), Bearing1_3['y'])
+    save_df(join(saved_dir, 'Bearing1_4_label_RUL'), Bearing1_4['y'])
+    save_df(join(saved_dir, 'Bearing1_5_label_RUL'), Bearing1_5['y'])
+    save_df(join(saved_dir, 'Bearing1_6_label_RUL'), Bearing1_6['y'])
+    save_df(join(saved_dir, 'Bearing1_7_label_RUL'), Bearing1_7['y'])
 
 
 # Load saved bearing data ==================================================================================
