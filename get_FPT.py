967ea9a5291f03bef072d24ff018e7cfc670d013
tdkhoa1212 <tdkhoa1212@gmail.com>
2022-12-18 18:35:11 +0700

    a

--- get_FPT.py
+++ get_FPT.py
@@ -61,35 +61,35 @@ else:
     Bearing1_5 = convert_to_image(Bearing1_5_path, opt, '1d', None, 'XJTU')
 
     #---------------------------------------------------------------------------------
-    Bearing2_1_path = join(main_dir_colab, '35Hz12kN', 'Bearing2_1')
+    Bearing2_1_path = join(main_dir_colab, '37.5Hz11kN', 'Bearing2_1')
     Bearing2_1 = convert_to_image(Bearing2_1_path, opt, '1d', None, 'XJTU')
 
-    Bearing2_2_path = join(main_dir_colab, '35Hz12kN', 'Bearing2_2')
+    Bearing2_2_path = join(main_dir_colab, '37.5Hz11kN', 'Bearing2_2')
     Bearing2_2 = convert_to_image(Bearing2_2_path, opt, '1d', None, 'XJTU')
     
-    Bearing2_3_path = join(main_dir_colab, '35Hz12kN', 'Bearing2_3')
+    Bearing2_3_path = join(main_dir_colab, '37.5Hz11kN', 'Bearing2_3')
     Bearing2_3 = convert_to_image(Bearing2_3_path, opt, '1d', None, 'XJTU')
 
-    Bearing2_4_path = join(main_dir_colab, '35Hz12kN', 'Bearing2_4')
+    Bearing2_4_path = join(main_dir_colab, '37.5Hz11kN', 'Bearing2_4')
     Bearing2_4 = convert_to_image(Bearing2_4_path, opt, '1d', None, 'XJTU')
 
-    Bearing2_5_path = join(main_dir_colab, '35Hz12kN', 'Bearing2_5')
+    Bearing2_5_path = join(main_dir_colab, '37.5Hz11kN', 'Bearing2_5')
     Bearing2_5 = convert_to_image(Bearing2_5_path, opt, '1d', None, 'XJTU')
 
     #---------------------------------------------------------------------------------
-    Bearing3_1_path = join(main_dir_colab, '35Hz12kN', 'Bearing3_1')
+    Bearing3_1_path = join(main_dir_colab, '40Hz10kN', 'Bearing3_1')
     Bearing3_1 = convert_to_image(Bearing3_1_path, opt, '1d', None, 'XJTU')
 
-    Bearing3_2_path = join(main_dir_colab, '35Hz12kN', 'Bearing3_2')
+    Bearing3_2_path = join(main_dir_colab, '40Hz10kN', 'Bearing3_2')
     Bearing3_2 = convert_to_image(Bearing3_2_path, opt, '1d', None, 'XJTU')
     
-    Bearing3_3_path = join(main_dir_colab, '35Hz12kN', 'Bearing3_3')
+    Bearing3_3_path = join(main_dir_colab, '40Hz10kN', 'Bearing3_3')
     Bearing3_3 = convert_to_image(Bearing3_3_path, opt, '1d', None, 'XJTU')
 
-    Bearing3_4_path = join(main_dir_colab, '35Hz12kN', 'Bearing3_4')
+    Bearing3_4_path = join(main_dir_colab, '40Hz10kN', 'Bearing3_4')
     Bearing3_4 = convert_to_image(Bearing3_4_path, opt, '1d', None, 'XJTU')
 
-    Bearing3_5_path = join(main_dir_colab, '35Hz12kN', 'Bearing3_5')
+    Bearing3_5_path = join(main_dir_colab, '40Hz10kN', 'Bearing3_5')
     Bearing3_5 = convert_to_image(Bearing3_5_path, opt, '1d', None, 'XJTU')
 
     data = {'Bearing1_1': Bearing1_1, 'Bearing1_2': Bearing1_2, 'Bearing1_3': Bearing1_3, 'Bearing1_4': Bearing1_4,
