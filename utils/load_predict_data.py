962638474defaa24bc9250abb14451f4d08e242f
tdkhoa1212 <tdkhoa1212@gmail.com>
2023-02-18 10:20:11 +0700

    a

--- utils/load_predict_data.py
+++ utils/load_predict_data.py
@@ -6,9 +6,6 @@ opt = parse_opt()
 np.random.seed(1234)
 
 # FPT of each bearing in both data-sets
-
-
-
 test_2D        = seg_data(load_df(opt.main_dir_colab + f'test_2D_{opt.condition}.pkz'), length)
 test_1D        = seg_data(load_df(opt.main_dir_colab + f'test_1D_{opt.condition}.pkz'), length)
 test_extract   = seg_data(load_df(opt.main_dir_colab + f'test_extract_{opt.condition}.pkz'), length)
