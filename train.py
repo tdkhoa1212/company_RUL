6bb711b62ee5851ecb0eacb95d65545cbbc10c0a
tdkhoa1212 <tdkhoa1212@gmail.com>
2023-02-24 11:34:23 +0700

    a

--- train.py
+++ train.py
@@ -27,8 +27,8 @@ def parse_opt(known=False):
    
     parser.add_argument('--save_dir',       default='/content/drive/MyDrive/Khoa/vibration_project/RUL/results', type=str)
     parser.add_argument('--data_type',      default=['2d', '1d', 'extract'], type=list, help='shape of data. They can be 1d, 2d, extract')
-    parser.add_argument('--train_bearing',  default=['Bearing1_1', 'Bearing1_2', 'Bearing2_1','Bearing2_2','Bearing3_1','Bearing3_2'], type=str, nargs='+')   
-    parser.add_argument('--test_bearing',   default=['Bearing1_3', 'Bearing1_4', 'Bearing1_5', 'Bearing2_3', 'Bearing2_4', 'Bearing2_5', 'Bearing3_3', 'Bearing3_4', 'Bearing3_5'], type=str, nargs='+')
+    parser.add_argument('--train_bearing',  default=['data002', 'data003','data004','data005'], type=str, nargs='+')   
+    parser.add_argument('--test_bearing',   default=['data001'], type=str, nargs='+')
     parser.add_argument('--scaler',         default='Normalizer', type=str)
     parser.add_argument('--main_dir_colab', default='/content/drive/MyDrive/Sensoteq Project/dataset', type=str, help="path to dataset")
 
